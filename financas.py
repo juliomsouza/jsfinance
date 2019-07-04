c643e7fa671ef52e8e755bea533181f4bbf2bb0a
Henrique Bastos <henrique@bastos.net>
2019-07-03 22:56:30 -0300

    Permite import do arquivo principal sem iniciar o Tk.

--- financas.py
+++ financas.py
@@ -1842,15 +1842,14 @@ class Sistema:
         
         
         
-        
-        
-root = Tk()
-Sistema(root)
-root.geometry("750x640+540+110")
-#root.resizable(0,0)
-root.title("JS Financas")
-root.configure(background='#004400')
-#root.configure(background='pink')
-root.configure(highlightbackground='#CC00CC')
-root.configure(highlightcolor='black')
-root.mainloop()
+if __name__ == '__main__':
+    root = Tk()
+    Sistema(root)
+    root.geometry("750x640+540+110")
+    #root.resizable(0,0)
+    root.title("JS Financas")
+    root.configure(background='#004400')
+    #root.configure(background='pink')
+    root.configure(highlightbackground='#CC00CC')
+    root.configure(highlightcolor='black')
+    root.mainloop()
